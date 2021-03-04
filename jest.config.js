e40b2f190b767e50704eed3ca04a5e4ae72b6a67
nanpangyou <yigehd@foxmail.com>
2021-03-04 18:04:53 +0800

    add: 增加jest对ts tsx的配置，测试的mock配置，添加classname的测试函数，安装了enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,22 @@
 //https://jestjs.io/docs/configuration.html   ==jest.config.js 文档地址
 
-
-
 module.exports = {
-    verbose: true,
-    clearMocks: false,
-    collectCoverage: false,
-    reporters: ['default'],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    moduleDirectories: ['node_modules'],
-    testMatch: ['<rootDir>/**/__tests__/**/*.test.(js|jsx|ts|tsx)'],
-    // setupFilesAfterEnv: ['<rootDir>test/setupTests.js']
+  verbose: true,
+  clearMocks: false,
+  collectCoverage: false,
+  reporters: ["default"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  moduleNameMapper: {
+    //https://www.jianshu.com/p/5d11cd50ead4   --jest配置
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/test/__mocks__/fileMock.js",
+    ".*\\.(css|less|scss)$": "<rootDir>/test/__mocks__/styleMock.js"
+  },
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleDirectories: ["node_modules"],
+  testMatch: ["<rootDir>/**/__tests__/**/*.test.(js|jsx|ts|tsx)"],
+  setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
 };
---
+++ lib/icon/__tests__/icon.test.tsx
@@ -0,0 +1,6 @@
+import Icon from "../icon";
+describe("icon组件", () => {
+  it("icon存在", () => {
+    expect(Icon).toBeTruthy;
+  });
+});
