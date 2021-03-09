80898b898a9433bf6ce0532d896bbd02e0d8c5eb
nanpangyou <yigehd@foxmail.com>
2021-03-09 17:55:51 +0800

    add: 简易展示网站

--- example.tsx
+++ example.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Icon from './lib/icon/icon'
+import "./lib/main.scss";
+import React, { Fragment } from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ReactDOM from "react-dom";
+import IconDemo from "./lib/icon/icon-demo";
+import DialogDemo from "./lib/dialog/dialog-demo";
+interface IRoutes {
+  path: string;
+  component: React.FunctionComponent;
+}
+const routes = [
+  {
+    path: "/icon",
+    component: IconDemo
+  }
+  // {
+  //   path: "/tacos",
+  //   component: Tacos,
+  //   routes: [
+  //     {
+  //       path: "/tacos/bus",
+  //       component: Bus
+  //     },
+  //     {
+  //       path: "/tacos/cart",
+  //       component: Cart
+  //     }
+  //   ]
+  // }
+];
 
-function App(): React.ReactElement {
-    return (<Icon name='loading' />)
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={(props) => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
 }
 
-ReactDOM.render(<App />, document.querySelector("#app"))
+function App(): React.ReactElement {
+  return (
+    <Fragment>
+      <header>PangUI</header>
+      <Router>
+        <aside>
+          <ul>
+            <li>
+              <Link to="/icon">icon</Link>
+            </li>
+          </ul>
+        </aside>
+        <main>
+          <Switch>
+            {routes.map((route, i) => (
+              <RouteWithSubRoutes key={i} {...route} />
+            ))}
+            {/* <Icon name="loading" />
+          <DialogDemo></DialogDemo> */}
+          </Switch>
+        </main>
+      </Router>
+      <footer>developed by nanpangyou</footer>
+    </Fragment>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
---
+++ lib/icon/icon-demo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Icon from "./icon";
+const Demo: React.FunctionComponent = (): React.ReactElement => {
+  return (
+    <React.Fragment>
+      <Icon name="setting" />
+      <Icon name="loading" />
+    </React.Fragment>
+  );
+};
+export default Demo;
